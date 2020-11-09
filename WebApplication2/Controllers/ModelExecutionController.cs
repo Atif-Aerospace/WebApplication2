fe37ab1c86b631a556ec1d8422dde1b0ea29ead4
Atif Riaz <a.riaz@cranfield.ac.uk>
2020-11-09 18:20:07 +0000

    Add project files.

---
+++ WebApplication2/Controllers/ModelExecutionController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+using System.IO;
+using Newtonsoft.Json.Linq;
+using WebApplication2.Execution;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ModelExecutionController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<ExecutionModel> GetData()
+        {
+            JObject requestBodyJson = null;
+
+            using (var reader = new StreamReader(Request.Body))
+            {
+                string requestBody = reader.ReadToEnd();
+                requestBodyJson = JObject.Parse(requestBody);
+            }
+
+
+
+            List<object> parameters = new List<object>();
+
+
+            ExecutionModel model = new ExecutionModel();
+
+            model.Name = (string)(requestBodyJson["Name"]);
+            //model.Description = "Add two numbers";
+            //model.Uri = "http://127.0.0.1:5000/ExecuteModel";
+
+            // Inputs
+            List<Data> inputs = new List<Data>();
+            var inputsJson = requestBodyJson["Inputs"];
+            for (int i = 0; i < inputsJson.Count(); i++)
+            {
+                Data data = new Data();
+                data.Name = (string)(inputsJson[i]["Name"]);
+                data.Value = (string)(inputsJson[i]["Value"]);
+                parameters.Add(Convert.ToDouble(data.Value));
+                inputs.Add(data);
+            }
+            model.Inputs = inputs;
+
+            // Outputs
+            List<Data> outputs = new List<Data>();
+            var outputsJson = requestBodyJson["Outputs"];
+            for (int i = 0; i < outputsJson.Count(); i++)
+            {
+                Data data = new Data();
+                data.Name = (string)(outputsJson[i]["Name"]);
+                data.Value = (string)(outputsJson[i]["Value"]);
+                parameters.Add(Convert.ToDouble(data.Value));
+                outputs.Add(data);
+            }
+            model.Outputs = outputs;
+
+
+
+            // Execute model
+            AircraftDesign em = new AircraftDesign();
+            Type type = em.GetType();
+            var methodInfo = type.GetMethod(model.Name);
+            object[] ppp = parameters.ToArray();
+            methodInfo.Invoke(em, ppp);
+
+
+
+
+            // Set outputs for json
+            int inputs_Count = inputsJson.Count();
+            for (int i = 0; i < outputs.Count(); i++)
+                outputs[i].Value = ppp[inputs_Count + i].ToString();
+
+            return model;
+        }
+    }
+
+
+
+    public class ExecutionModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public string Uri { get; set; }
+
+        public List<Data> Inputs { get; set; }
+
+        //public List<Data> Inputs = new List<Data>();
+        public List<Data> Outputs { get; set; }
+    }
+    public class Data
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ WebApplication2/Execution/AircraftDesign.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Execution
+{
+    public class AircraftDesign
+    {
+        public void Power(double d, double v, out double p)
+        {
+            p = d * d * v * v * v / 2000;
+        }
+        public void Cost(double p, double d, out double c)
+        {
+            c = p * d;
+        }
+        public void Life(double p, double d, out double l)
+        {
+            l = 100 / Math.Exp(Math.Sqrt((p + d / 10) / 100));
+        }
+
+        public void AddNumbers(double x1, double x2, out double y1)
+        {
+            y1 = x1 + x2;
+        }
+
+        public void MultiplyNumbers(double x1, double x2, out double y1)
+        {
+            y1 = x1 * x2;
+        }
+    }
+}
